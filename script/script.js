e8dcc562e8f081229755bbb73f139236fe6ffde3
Shama Nurlanov <esenkozhoev99@inbox.ru>
2021-03-15 07:56:32 +0600

    m

--- script/script.js
+++ script/script.js
@@ -7,21 +7,16 @@ menuBtn.onclick = function()
     this.classList.toggle('menuBtn_active');
 }
 
-document.querySelector('.preloader').style.transform = 'translateY(-100%)';
-document.querySelector('nav').style.top = 0;
-
 window.onload = function(){
     const letters = document.querySelectorAll('.letters')
     var current = 0;
-        let timer = setInterval(function(){
+    let timer = setInterval(function(){
              letters[current].style.display = 'inline';
             current++;
-            if(current >= letters.length) {
-                clearInterval(timer);
-                document.querySelector('.preloader').style.transform = 'translateY(-100%)';
-                document.querySelector('nav').style.top = 0;
-            }
-        }, 500);
-
+            if(current >= letters.length) clearInterval(timer);
+        }, 800);
+    setTimeout(function(){
+        document.querySelector('.preloader').style.transform = 'translateY(-100%)';
+        document.querySelector('nav').style.top = 0;
+    }, 4000); 
 }
-
